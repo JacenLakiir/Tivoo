345e5684c312a70bfcf028af8d27d6c35aaea6ed
Hui Dong <huid.bupt@gmail.com>
2012-02-10 14:23:17 -0500

    First changes

--- src/prototypes/PrototypeEricandHui.java
+++ src/prototypes/PrototypeEricandHui.java
@@ -1,6 +1,12 @@
 package prototypes;
 
-public class PrototypeEricandHui
-{
 
-}
+import javax.xml.parsers.SAXParser;
+import com.hp.*;
+import java.util.Calendar;
+
+public class PrototypeEricandHui{
+ SAXParser parser;
+ 
+ 
+} 
