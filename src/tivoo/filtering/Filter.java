7fc68b0c222af75e79cc7ae0bb6db45ab4871807
Eric Mercer <ericwjm@gmail.com>
2012-02-12 14:46:02 -0500

    Defined methods for Filter interface

--- src/tivoo/filtering/Filter.java
+++ src/tivoo/filtering/Filter.java
@@ -6,6 +6,13 @@ import java.util.List;
 public interface Filter
 {
     
+    /**
+     * Finds all events with the given keyword in its title.
+     */
     List<Event> filterByKeyword (String keyword, List<Event> current);
         
+    /**
+     * Finds all events for a given week.
+     */
+    List<Event> filterByWeek (String week, List<Event> current);
 }
