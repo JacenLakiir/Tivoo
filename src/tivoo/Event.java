4dcf6c7100afbe7f0f7513c54751a7d6956f1542
Eric Mercer <ericwjm@gmail.com>
2012-02-12 14:37:07 -0500

    Defined methods for Event interface

--- src/tivoo/Event.java
+++ src/tivoo/Event.java
@@ -3,4 +3,14 @@ package tivoo;
 public interface Event
 {
 
+    public String getTitle();
+    
+    public String getDescription ();
+    
+    public String getStartTime ();
+    
+    public String getEndTime ();
+    
+    public String getLocation ();
+    
 }
