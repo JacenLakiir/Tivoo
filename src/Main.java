4ec16185dc11c791129a504d365e87076fbceb72
Eric Mercer <ericwjm@gmail.com>
2012-02-27 04:39:08 -0500

    Implemented simple version of calendar view for output.
    Will be better formatted and designed in a future commit.

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main
 
         System.out.println("Loading XML input...");
         s.loadFile("data/dukecal.xml");
-
+        
         System.out.println("Filtering events...");
         s.filterByKeyword("Lecture");
         s.sortByTitle(false);
--- src/tivoo/output/CalendarViewHTMLBuilder.java
+++ src/tivoo/output/CalendarViewHTMLBuilder.java
@@ -1,8 +1,8 @@
 package tivoo.output;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Map;
+import java.util.List;
 import com.hp.gagawa.java.Document;
 import com.hp.gagawa.java.elements.*;
 import tivoo.Event;
@@ -11,16 +11,15 @@ public class CalendarViewHTMLBuilder extends HTMLBuilder
 {
     
     private static final String TITLE = "Calendar View";
-    private static final String UNIQUE_CSS = "../css/calendarStyle.css";
-    
-    private static final long dayLength = 86400000L;
-    private static final long weekLength = 604800000L;
-    
+    private static final String UNIQUE_CSS = "../css/calendarStyle.css";    
+    private static final long DAY_LENGTH = 86400000L;
+    private static final long WEEK_LENGTH = 604800000L;
+        
     private enum Type
     {
         DAY, WEEK, MONTH
     }
-    
+        
     public CalendarViewHTMLBuilder (String summaryPageFileName)
     {
         super(summaryPageFileName);
@@ -29,10 +28,12 @@ public class CalendarViewHTMLBuilder extends HTMLBuilder
     @Override
     protected void writeSummaryPageContent (Document doc, List<Event> eventList)
     {
-//        Div content = new Div().setCSSClass("content");     
-//        content.appendChild(new H3().appendText(getTitle()));
-//        content.appendChild(buildCalendar(eventList));
-//        doc.body.appendChild(content);
+        Div content = new Div().setCSSClass("content");     
+        content.appendChild(new H3().appendText(TITLE));
+        
+        Div calendarView = buildCalendar(eventList);
+        content.appendChild(calendarView);
+        doc.body.appendChild(content);
     }
     
     @Override
@@ -47,123 +48,121 @@ public class CalendarViewHTMLBuilder extends HTMLBuilder
         return UNIQUE_CSS;
     }
     
-//    private Table buildCalendar (List<Event> eventList)
-//    {
-//        Table view = initializeCalendar();
-//        Type calendarType = determineCalendarType(eventList);
-//        Map<String, List<Event>> sortedEvents = sortByDate(eventList);
-//        if (calendarType.equals(Type.DAY))
-//        {
-//            String day = getDay(eventList.get(0));
-//            view.appendChild(buildDayCalendar(day, sortedEvents));
-//        }
-//        else if (calendarType.equals(Type.WEEK))
-//        {
-//            buildWeekCalendar(view, sortedEvents);
-//        }
-//        else
-//        {
-//            buildMonthCalendar(view, sortedEvents);
-//        }
-//        return view;
-//    }
-//
-//    private Table initializeCalendar ()
-//    {
-//        Table view = new Table();
-//        view.setTitle("Calendar View");
-//        view.setCellspacing("0");
-//        
-//        Tr tableHeading = buildTableHeading();
-//        view.appendChild(tableHeading);
-//        return view;
-//    }
-//
-//    private Type determineCalendarType (List<Event> eventList)
-//    {
-//        Event first = eventList.get(0);
-//        Event last = eventList.get(0); 
-//        for (Event e : eventList)
-//        {
-//            Calendar start = e.getStartTime();
-//            first = (start.getTimeInMillis() < first.getStartTime().getTimeInMillis()) ? e : first;
-//            
-//            Calendar end = e.getEndTime();
-//            last = (end.getTimeInMillis() > last.getEndTime().getTimeInMillis()) ? e : last;
-//        }
-//        long timeframe = last.getEndTime().getTimeInMillis() - first.getStartTime().getTimeInMillis();
-//        if (timeframe <= dayLength)
-//        {
-//            return CalendarViewHTMLBuilder.Type.DAY;
-//        }
-//        if (timeframe <= weekLength)
-//        {
-//            return CalendarViewHTMLBuilder.Type.WEEK;
-//        }
-//        return CalendarViewHTMLBuilder.Type.MONTH;
-//    }
-//    
-//    private Tr buildTableHeading ()
-//    {
-//        Tr headingRow = new Tr();
-//        for (String day : daysList)
-//        {
-//            Th heading = new Th().setCSSClass("heading").appendText(day);
-//            headingRow.appendChild(heading);
-//        }
-//        return headingRow;
-//    }
-//    
-//    private Tr buildTableRow (Map<String, List<Event>> sortedEvents)
-//    {
-//        Tr tableRow = new Tr();
-//        for (String day : daysList)
-//        {
-//            List<Event> eventsOnThisDay = sortedEvents.get(day);
-//            Td dayCal = buildDayCalendar(day, eventsOnThisDay);
-//            tableRow.appendChild(dayCal);
-//        }
-//        return tableRow;
-//    }
-//    
-//    private void buildMonthCalendar (Table view, Map<String, List<Event>> sortedEvents)
-//    {
-//        for (int i = 0; i < 5; i++)
-//        {
-//            Tr tableRow = buildTableRow(sortedEvents);
-//            view.appendChild(tableRow);
-//        }
-//    }
-//    
-//    private void buildWeekCalendar (Table view, Map<String, List<Event>> sortedEvents2)
-//    {        
-//        Map<String, List<Event>> sortedEvents = sortByDayOfWeek(sortedEvents2);
-//        Tr tableRow = buildTableRow(sortedEvents);
-//        view.appendChild(tableRow);
-//    }
-//
-//    private Td buildDayCalendar (String day, Map<String, List<Event>> sortedEvents)
-//    {
-//        boolean columnClass = (daysList.indexOf(day) % 2 == 0);
-//        Td dayCal = columnClass ? new Td().setCSSClass("gr1") : new Td().setCSSClass("gr1alt");
-//        Div dayDiv = constructDayDiv(sortedEvents);
-//        dayCal.appendChild(dayDiv);
-//        return dayCal;
-//    }
-//
-//    private Div constructDayDiv (List<Event> eventsOnThisDay)
-//    {
-//        Div dayEvents = new Div();
-//        dayEvents.setId("dayEvents");
-//        if (eventsOnThisDay != null)
-//        {
-//            for (Event currentEvent : eventsOnThisDay)
-//            {
-//                Div eventInfo = constructEventDiv(currentEvent);
-//                dayEvents.appendChild(eventInfo);
-//            }
-//        }
-//        return dayEvents;
-//    }
-//    
+    private Div buildCalendar (List<Event> eventList)
+    {
+        Div calendarView = new Div().setId("calendarView");
+        Type calendarType = determineCalendarType(eventList);
+        if (calendarType.equals(Type.MONTH))
+        {
+            Ul monthCalendar = buildMonthCalendar(eventList);
+            calendarView.appendChild(monthCalendar);
+        }
+        if (calendarType.equals(Type.WEEK))
+        {
+            Ul weekCalendar = buildWeekCalendar(eventList);
+            calendarView.appendChild(weekCalendar);
+        }
+        if (calendarType.equals(Type.DAY))
+        {
+            Ul dayCalendar = buildDayCalendar(eventList);
+            calendarView.appendChild(dayCalendar);
+        }
+        return calendarView;
+    }
+    
+    private Type determineCalendarType (List<Event> eventList)
+    {
+        Event first = eventList.get(0);
+        Event last = eventList.get(0); 
+        for (Event e : eventList)
+        {
+            Calendar start = e.getStartTime();
+            first = (start.getTimeInMillis() < first.getStartTime().getTimeInMillis()) ? e : first;
+            
+            Calendar end = e.getEndTime();
+            last = (end.getTimeInMillis() > last.getEndTime().getTimeInMillis()) ? e : last;
+        }
+        long timeframe = last.getEndTime().getTimeInMillis() - first.getStartTime().getTimeInMillis();
+        if (timeframe <= DAY_LENGTH)
+            return CalendarViewHTMLBuilder.Type.DAY;
+        if (timeframe <= WEEK_LENGTH)
+            return CalendarViewHTMLBuilder.Type.WEEK;
+        return CalendarViewHTMLBuilder.Type.MONTH;
+    }
+
+    private Ul buildDayCalendar (List<Event> eventList)
+    {
+        Ul dayCalendar = new Ul().setId("dayCal");
+        for (Event e : eventList)
+        {
+            Li eventInfo = constructEventItem(e);
+            dayCalendar.appendChild(eventInfo);
+        }
+        return dayCalendar;
+    }
+    
+    private Ul buildWeekCalendar (List<Event> eventList)
+    {
+        Ul weekCalendar = new Ul().setId("weekCal");
+        Map<String, List<Event>> sortedEvents = sortByDayOfWeek(eventList);
+        for (String day : DAYS_LIST)
+        {
+            Ul dayInfo = new Ul().setId("dayInfo").appendChild(new B().appendText(day));
+            List<Event> eventsOnThisDay = sortedEvents.get(day);
+            if (eventsOnThisDay != null)
+            {
+                for (Event e : eventsOnThisDay)
+                {
+                    Li eventInfo = constructEventItem(e);
+                    dayInfo.appendChild(eventInfo);
+                }
+            }
+            else
+            {
+                dayInfo.appendChild(new Li().appendChild(new P().appendText("None")));
+            }
+            weekCalendar.appendChild(new Li().appendChild(dayInfo));
+        }
+        return weekCalendar;
+    }
+    
+    private Ul buildMonthCalendar (List<Event> eventList)
+    {
+        Ul monthCalendar = new Ul().setId("monthCal");
+        Map<String, List<Event>> sortedEvents = sortByDate(eventList);
+        for (Event e: eventList)
+        {
+            String date = getDate(e);
+            Ul dateInfo = new Ul().setId("dateInfo").appendChild(new B().appendText(date));
+            List<Event> eventsOnThisDate = sortedEvents.get(date);
+            if (eventsOnThisDate != null)
+            {
+                for (Event currentEvent : eventsOnThisDate)
+                {
+                    Li eventInfo = constructEventItem(currentEvent);
+                    dateInfo.appendChild(eventInfo);
+                }
+            }
+            else
+            {
+                dateInfo.appendChild(new Li().appendChild(new P().appendText("None")));
+            }
+            monthCalendar.appendChild(new Li().appendChild(dateInfo));
+        }
+        return monthCalendar;
+    }
+    
+    private Li constructEventItem (Event currentEvent)
+    {
+        Li eventInfo = new Li().setId("event");
+        
+        P eventP = new P();
+        eventP.appendChild(linkToDetailsPage(currentEvent));
+        eventP.appendChild(new Br());
+        eventP.appendText(formatClockTimespan(currentEvent));
+        
+        eventInfo.appendChild(eventP);
+        return eventInfo;
+    }
+    
 }
